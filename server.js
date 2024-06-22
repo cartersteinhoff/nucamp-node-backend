32349db569d94968cf962fc9a4cb404c467c7537
Carter Steinhoff <cartersteinhoff@gmail.com>
2024-06-22 08:02:20 -0700

    removed node modules

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const hostname = "localhost";
 const port = 3000;
 
 const app = express();
-
 app.use(morgan("dev"));
+app.use(express.json());
 
+app.use("/campsites", campsiteRouter);
 app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
