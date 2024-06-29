11f05424e850450e2c05b4d4bc7bba399f924af6
Carter Steinhoff <cartersteinhoff@gmail.com>
2024-06-29 08:33:04 -0700

    First commit

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
+
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/nucampsite";
+const connect = mongoose.connect(url, {});
+
+connect.then(
+  () => console.log("Connected correctly to server"),
+  (err) => console.log(err)
+);
 
 const hostname = "localhost";
 const port = 3000;
@@ -9,6 +21,9 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partner", partnerRouter);
+
 app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
